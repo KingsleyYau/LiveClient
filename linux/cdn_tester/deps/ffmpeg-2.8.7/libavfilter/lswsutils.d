95eab95d4f159affad875ce8d97af4117b50c959
Kingsley Yau <Kingsleyyau@gmail.com>
2017-07-05 16:01:09 +0800

    删除第三方解压包

--- linux/cdn_tester/deps/ffmpeg-2.8.7/libavfilter/lswsutils.d
+++
@@ -1,9 +0,0 @@
-libavfilter/lswsutils.o: libavfilter/lswsutils.c libavutil/imgutils.h \
-  libavutil/avutil.h libavutil/common.h libavutil/attributes.h \
-  libavutil/version.h libavutil/macros.h libavutil/avconfig.h config.h \
-  libavutil/intmath.h libavutil/mem.h libavutil/error.h \
-  libavutil/internal.h libavutil/timer.h libavutil/log.h libavutil/cpu.h \
-  libavutil/dict.h libavutil/pixfmt.h libavutil/old_pix_fmts.h \
-  libavutil/libm.h libavutil/intfloat.h libavutil/rational.h \
-  libavutil/mathematics.h libavutil/pixdesc.h libavfilter/lswsutils.h \
-  libswscale/swscale.h libswscale/version.h
--- linux/cdn_tester/deps/ffmpeg-2.8.7/libavutil/mathematics.d
+++
@@ -1,8 +0,0 @@
-libavutil/mathematics.o: libavutil/mathematics.c libavutil/mathematics.h \
-  libavutil/attributes.h libavutil/rational.h libavutil/intfloat.h \
-  libavutil/common.h libavutil/version.h libavutil/macros.h \
-  libavutil/avconfig.h config.h libavutil/intmath.h libavutil/mem.h \
-  libavutil/error.h libavutil/avutil.h libavutil/log.h \
-  libavutil/pixfmt.h libavutil/old_pix_fmts.h libavutil/internal.h \
-  libavutil/timer.h libavutil/cpu.h libavutil/dict.h libavutil/libm.h \
-  libavutil/avassert.h
